8b0ec7f320de0439fe1fc00d8f0d1f07d52591a2
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-08-21 19:14:41 +1000

    updated

--- slippylib.py
+++ slippylib.py
@@ -2,12 +2,6 @@ import serial
 import threading
 import time
 
-## Defs
-MESSAGE_TAG = "[MESSAGE]"
-ADDRESS_TAG = "[ADDRESS]"
-ERROR_TAG   = "[ERROR]"
-INFO_TAG    = "[INFO]"
-
 class SlipPy:
     def __init__(self, port, baudrate=115200):
         self.port = port
@@ -26,31 +20,31 @@ class SlipPy:
     def on_message(self, dest_address, src_address, rssi, snr, msg_id, message): pass # print raw message
     def on_error(self, error): pass
     def on_info(self, info): pass
+    def on_ready(self, addr): pass
 
     def __receive(self):
         while True:
             line = self.device.readline().decode().strip()
             if line:
-                split_msg = line.split(";")
+                #print(line)
+                split_msg = line.split(" ")
 
-                if split_msg[0] == MESSAGE_TAG:
+                if split_msg[0] == "MESSAGE":
                     dest_address = split_msg[1]
                     src_address = split_msg[2]
-                    rssi = split_msg[3]
-                    snr = split_msg[4]
-                    msg_id = split_msg[5]
-                    msg_len = split_msg[6]
+                    msg_id = split_msg[3]
+                    msg_len = split_msg[4]
+                    message = split_msg[5].split(",")
+                    rssi = split_msg[6]
+                    snr = split_msg[7]
 
-                    self.on_message(dest_address, src_address, rssi, snr, msg_id, "".join(str(part) for part in split_msg[7:]))
+                    self.on_message(dest_address, src_address, rssi, snr, msg_id, "".join(str(chr(int(char))) for char in message))
 
-                if split_msg[0] == ADDRESS_TAG:
-                    self.address = split_msg[1]
+                if split_msg[0] == "READY":
+                    self.on_ready = split_msg[1]
                 
-                if split_msg[0] == ERROR_TAG:
-                    self.on_error(split_msg[1])
-                
-                if split_msg[0] == INFO_TAG:
-                    self.on_info(split_msg[1])
 
     def send(self, address, message):
-        self.device.write(f"{address};{message}\0".encode("ascii"))
+        if (len(message) > 128):
+            return "message too big"
+        self.device.write(f"0 {' '.join(str(id) for id in address.split('.'))} {len(message)} {' '.join(str(ord(char)) for char in message)} 0\n".encode("ascii"))
